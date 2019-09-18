2bdd87d8c5009c3f0dab4e1f53bccb07072ced99
miguel <miguelesquirol@gmail.com>
2019-09-17 22:43:02 -0400

    map multiple markers

--- free-now-test-frontend/src/components/map.js
+++ free-now-test-frontend/src/components/map.js
@@ -1,55 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 export class MapArea extends React.Component {
-   
+ 
+    state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+  
+    onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+  
+    onMapClicked = (props) => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        })
+      }
+    };
+  
+    render() {
+      return (
+        <Map google={this.props.google}
+            onClick={this.onMapClicked}>
 
-  constructor(props) {
-    super(props);
+      <Marker
+        title={'The marker`s title will appear as a tooltip.'}
+        name={'SOMA'}
+        position={{lat: 37.778519, lng: -122.405640}} />
+      <Marker
+        name={'Dolores park'}
+        position={{lat: 37.759703, lng: -122.428093}} />
+      <Marker />
 
-    this.state = {
-      stores: [{latitude: 47.49855629475769, longitude: -122.14184416996333},
-              {latitude: 47.359423, longitude: -122.021071},
-              {latitude: 47.2052192687988, longitude: -121.988426208496},
-              {latitude: 47.6307081, longitude: -122.1434325},
-              {latitude: 47.3084488, longitude: -122.2140121},
-              {latitude: 47.5524695, longitude: -122.0425407}]
+  
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+  
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      )
     }
   }
 
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
-    })
-  }
-
-    render() {
-
-    const mapStyles = {
-        width: '100%',
-        height: '100%',
-      };
-
-      
-    return (<div> 
-         The data from parent is {this.props.latfromjson} {this.props.lonfromjson}
-    
-            <Map
-          google={this.props.google}
-          zoom={8}
-          style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        >
-        {this.displayMarkers()}
-        </Map></div>
-  );
-};
-};
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyCVm1D8yiD0ZbKNIt1ObzdkzZoutb1y1oE'
